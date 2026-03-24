9221ac354a5e4589b649eb69164bb584eff91c7d
tanssixyz <happydancer@tanssi.xyz>
2026-03-25 01:18:12 +0200

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client"
 import { WagmiProvider } from "wagmi"
 import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Analytics } from "@vercel/analytics/react"
 import "@rainbow-me/rainbowkit/styles.css"
 import "./index.css"
 import App from "./App.tsx"
@@ -23,5 +24,6 @@ createRoot(document.getElementById("root")!).render(
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
+    <Analytics />
   </StrictMode>
 )
